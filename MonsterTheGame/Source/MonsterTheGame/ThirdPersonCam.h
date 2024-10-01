6ac029ed625dfdbe7d675254c75adbe803c779af
GuneetSasan <WhyDoICode@gmail.com>
2024-10-01 01:37:03 -0600

    Migration From TheMonsterGame  B >

---
+++ MonsterTheGame/Source/MonsterTheGame/ThirdPersonCam.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ATPCCameraComponent.h"
+
+#include "ThirdPersonCam.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, meta = (BlueprintSpawnableComponent))
+class MONSTERTHEGAME_API UThirdPersonCam : public UATPCCameraComponent
+{
+	GENERATED_BODY()
+};
